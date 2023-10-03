64d424f0edc6d761cf5d849168dbd24c581cd9c7
Samuel <salvadomo@hotmail.com>
2023-10-02 20:32:48 -0600

    added button function

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+var searchBtn = document.getElementById('searchBtn');
+
+function search() {
+    console.log('Button was clicked!')
+    event.preventDefault();
+    }
+searchBtn.addEventListener('click', search);
