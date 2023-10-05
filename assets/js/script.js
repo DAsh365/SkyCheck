a58dfacb80d5c2bc25c1f90f793ece76ad587d2c
Jared P <jared.t.paden@gmail.com>
2023-10-04 18:48:01 -0600

    Removed await, comparing CSS w/ group

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 var searchBtn = document.getElementById('searchBtn');
-
-function search() {
+// Uses the search button (from HTML id 'searchBtn') to start a function
+function search(event) {
     console.log('Button was clicked!')
     event.preventDefault();
     }
@@ -9,21 +9,21 @@ searchBtn.addEventListener('click', search);
 const axios = require('axios');
 
 const options = {
-  method: 'GET',
-  url: 'https://travel-advisor.p.rapidapi.com/locations/v2/auto-complete',
-  params: {
+method: 'GET',
+url: 'https://travel-advisor.p.rapidapi.com/locations/v2/auto-complete',
+params: {
     query: 'Alabama - Montgomery, Alaska - Juneau, Arizona - Phoenix, Arkansas - Little Rock, California - Sacramento, Colorado - Denver, Connecticut - Hartford, Delaware - Dover, Florida - Tallahassee, Georgia - Atlanta, Hawaii - Honolulu, Idaho - Boise, Illinois - Springfield, Indiana - Indianapolis, Iowa - Des Moines, Kansas - Topeka, Kentucky - Frankfort, Louisiana - Baton Rouge, Maine - Augusta, Maryland - Annapolis, Massachusetts - Boston, Michigan - Lansing, Minnesota - Saint Paul, Mississippi - Jackson, Missouri - Jefferson City, Montana - Helena, Nebraska - Lincoln, Nevada - Carson City, New Hampshire - Concord, New Jersey - Trenton, New Mexico - Santa Fe, New York - Albany, North Carolina - Raleigh, North Dakota - Bismarck, Ohio - Columbus, Oklahoma - Oklahoma City, Oregon - Salem, Pennsylvania - Harrisburg, Rhode Island - Providence, South Carolina - Columbia, South Dakota - Pierre, Tennessee - Nashville, Texas - Austin, Utah - Salt Lake City, Vermont - Montpelier, Virginia - Richmond, Washington - Olympia, West Virginia - Charleston, Wisconsin - Madison, Wyoming - Cheyenne',
     lang: 'en_US',
     units: 'mi'
-  },
-  headers: {
+},
+headers: {
     'X-RapidAPI-Key': '32f2355e22msh06cc94cd6adf337p1b96abjsnb61d184ee648',
     'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
-  }
+}
 };
 
 try {
-	const response = await axios.request(options);
+	const response = axios.request(options);
 	console.log(response.data);
 } catch (error) {
 	console.error(error);
