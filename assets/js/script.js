9b306a37b7be85284494a71b5339c3e25941464f
Devan Ash <141689897+DAsh365@users.noreply.github.com>
2023-10-05 18:44:07 -0600

    moving java

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,60 @@
 var searchBtn = document.getElementById('searchBtn');
+
+
 // Uses the search button (from HTML id 'searchBtn') to start a function
 function search(event) {
     console.log('Button was clicked!')
     event.preventDefault();
     }
-searchBtn.addEventListener('click', search);
 
-const axios = require('axios');
+    searchBtn.addEventListener('click', search);
 
-const options = {
-method: 'GET',
-url: 'https://travel-advisor.p.rapidapi.com/locations/v2/auto-complete',
-params: {
-    query: 'Alabama - Montgomery, Alaska - Juneau, Arizona - Phoenix, Arkansas - Little Rock, California - Sacramento, Colorado - Denver, Connecticut - Hartford, Delaware - Dover, Florida - Tallahassee, Georgia - Atlanta, Hawaii - Honolulu, Idaho - Boise, Illinois - Springfield, Indiana - Indianapolis, Iowa - Des Moines, Kansas - Topeka, Kentucky - Frankfort, Louisiana - Baton Rouge, Maine - Augusta, Maryland - Annapolis, Massachusetts - Boston, Michigan - Lansing, Minnesota - Saint Paul, Mississippi - Jackson, Missouri - Jefferson City, Montana - Helena, Nebraska - Lincoln, Nevada - Carson City, New Hampshire - Concord, New Jersey - Trenton, New Mexico - Santa Fe, New York - Albany, North Carolina - Raleigh, North Dakota - Bismarck, Ohio - Columbus, Oklahoma - Oklahoma City, Oregon - Salem, Pennsylvania - Harrisburg, Rhode Island - Providence, South Carolina - Columbia, South Dakota - Pierre, Tennessee - Nashville, Texas - Austin, Utah - Salt Lake City, Vermont - Montpelier, Virginia - Richmond, Washington - Olympia, West Virginia - Charleston, Wisconsin - Madison, Wyoming - Cheyenne',
-    lang: 'en_US',
-    units: 'mi'
-},
-headers: {
-    'X-RapidAPI-Key': '32f2355e22msh06cc94cd6adf337p1b96abjsnb61d184ee648',
-    'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
-}
-};
+    //const url = 'https://tripadvisor16.p.rapidapi.com/api/v1/flights/searchFlights?sourceAirportCode=DEN&destinationAirportCode=AUS&date=2023-10-05&itineraryType=ONE_WAY&sortOrder=PRICE&numAdults=1&numSeniors=0&classOfService=ECONOMY&returnDate=2023-10-11&pageNumber=1&currencyCode=USD';
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': '32f2355e22msh06cc94cd6adf337p1b96abjsnb61d184ee648',
+		'X-RapidAPI-Host': 'tripadvisor16.p.rapidapi.com'
+      }
+    };
 
-try {
-	const response = axios.request(options);
-	console.log(response.data);
-} catch (error) {
-	console.error(error);
-}
+
+
+
+    fetch(url, options)
+	.then(function (res) {
+		return res.json();
+	})
+	.then(function (data) {
+		console.log(data);
+		getData(data);
+	})
+	.catch(function (error) {
+		console.log(error);
+	});
+
+  function getData(results) {
+    var price = results.data.flights[0].purchaseLinks[0].totalPrice;
+  
+    var stops = results.data.flights[0].segments[0].legs[0].numStops;
+  
+    var departAirport =
+      results.data.flights[0].segments[0].legs[0].originStationCode;
+  
+    var arriveAirport =
+      results.data.flights[0].segments[0].legs[0].destinationStationCode;
+  
+    var flightNumber = results.data.flights[0].segments[0].legs[0].flightNumber;
+  
+    var carrier =
+      results.data.flights[0].segments[0].legs[0].operatingCarrier.displayName;
+  
+    var departTime =
+      results.data.flights[0].segments[0].legs[0].departureDateTime;
+  
+    var arrivalTime = results.data.flights[0].segments[0].legs[0].arrivalDateTime;
+  
+    console.log(
+      `Price: ${price} \n Stops: ${stops} \n Departure: ${departAirport} \n Arrival: ${arriveAirport} \n Flight Number: ${flightNumber} \n Carrier: ${carrier} \n Departure Time: ${departTime} \n Arrival Time ${arrivalTime}`
+    );
+  }
